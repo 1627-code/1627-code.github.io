29f685b79ae5fc92b6378313c8c1276b5421ca11
1627-code <everobenny1627@gmail.com>
2025-03-07 14:37:25 +0100

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,23 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("posts.json")
-        .then(response => response.json())
-        .then(posts => {
-            const blogContainer = document.getElementById("blog-posts");
+document.addEventListener("DOMContentLoaded", function () {
+    const blogPostsContainer = document.getElementById("blogPosts");
+    let blogs = JSON.parse(localStorage.getItem("blogs")) || [];
 
-            posts.forEach(post => {
-                const blogCard = document.createElement("div");
-                blogCard.classList.add("blog-card");
+    if (blogs.length === 0) {
+        blogPostsContainer.innerHTML = "<p>No blog posts yet. <a href='write-blog.html'>Write your first blog!</a></p>";
+    } else {
+        blogs.forEach(blog => {
+            const blogCard = document.createElement("div");
+            blogCard.classList.add("blog-card");
 
-                blogCard.innerHTML = `
-                    <h3>${post.title}</h3>
-                    <p class="blog-date">${post.date}</p>
-                    <p>${post.summary}</p>
-                    <a href="${post.link}" class="read-more">Read More</a>
-                `;
+            blogCard.innerHTML = `
+                <h3>${blog.title}</h3>
+                <p>${blog.description.substring(0, 100)}...</p>
+                <span class="blog-date">${blog.date}</span>
+                <a href="blog-details.html?id=${blog.id}" class="read-more">Read More</a>
+            `;
 
-                blogContainer.appendChild(blogCard);
-            });
-        })
-        .catch(error => console.error("Error loading posts:", error));
+            blogPostsContainer.appendChild(blogCard);
+        });
+    }
 });
-
